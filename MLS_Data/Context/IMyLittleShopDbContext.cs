47d1c06468cdc0c34927dc8e807e39bb2ab52ac6
Enes Can UYAR <uyar.enescan@gmail.com>
2022-02-07 21:56:08 +0300

    Add context

---
+++ MLS_Data/Context/IMyLittleShopDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MLS_Data.DataModels;
+
+namespace MLS_Data.Context
+{
+    public interface IMyLittleShopDbContext
+    {
+        DbSet<User_DataModel> Users { get; set; }
+        DbSet<Category_DataModel> Categories { get; set; }
+        DbSet<Product_DataModel> Products { get; set; }
+    }
+}
---
+++ MLS_Data/Context/MyLittleShopDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MLS_Data.DataModels;
+
+namespace MLS_Data.Context
+{
+    public class MyLittleShopDbContext : DbContext, IMyLittleShopDbContext
+    {
+        public MyLittleShopDbContext(DbContextOptions<MyLittleShopDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User_DataModel> Users { get; set; }
+        public DbSet<Category_DataModel> Categories { get; set; }
+        public DbSet<Product_DataModel> Products { get; set; }
+    }
+}
