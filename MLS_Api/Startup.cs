3aae8f076fce72ebf02f539b761825ce19aa03e1
Enes Can UYAR <uyar.enescan@gmail.com>
2022-02-08 15:28:17 +0300

    Connection to the database

--- MLS_Api/Startup.cs
+++ MLS_Api/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MLS_Data.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,10 @@ namespace MLS_Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //connection to database
+            var connectionString = Configuration.GetConnectionString("MyLittleShopDatabaseString");
+            services.AddDbContext<MyLittleShopDbContext>(options => options.UseSqlServer(connectionString));
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
